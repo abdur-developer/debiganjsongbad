2c93d5ecc37982dfa159df4d46cdc0a986a10391
abdur-git <abdurr3336@gmail.com>
2026-03-27 18:06:46 +0600

    ads real

--- my_ads/index.php
+++ my_ads/index.php
@@ -108,9 +108,8 @@ require_once 'ad_functions.php';
             }
         </style>
         
-        <!-- Google AdSense -->
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-            crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4520141412693223"
+     crossorigin="anonymous"></script>
     </head>
     <body>
         <div class="container">
